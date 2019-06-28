db8cf3ad8fb6417e9b18700a5a55eae10ebc3e4e
284831550 <52198985+284831550@users.noreply.github.com>
2019-06-28 09:28:23 +0800

    Update Item.java

--- sp01-commons/src/main/java/com/tedu/sp01/pojo/Item.java
+++ sp01-commons/src/main/java/com/tedu/sp01/pojo/Item.java
@@ -3,7 +3,7 @@ package com.tedu.sp01.pojo;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
+//修改11111111111
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
