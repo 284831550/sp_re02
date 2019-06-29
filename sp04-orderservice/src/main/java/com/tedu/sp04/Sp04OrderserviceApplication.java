142d508fc568526911cffd28fe8545f679937a96
UID <UID@DESKTOP-DACJOCN>
2019-06-29 09:45:06 +0800

    11111

---
+++ sp04-orderservice/src/main/java/com/tedu/sp04/Sp04OrderserviceApplication.java
@@ -0,0 +1,16 @@
+package com.tedu.sp04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+//@EnableDiscoveryClient服务注册发现
+//@SpringBootApplication
+@EnableFeignClients
+@SpringCloudApplication
+public class Sp04OrderserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sp04OrderserviceApplication.class, args);
+	}
+
+}
